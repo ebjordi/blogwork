848b03a325d91bb772e2042322eda8fb126d3e95
Jordi Eguren Brown <jordi@fcaglp.unlp.edu.ar>
2023-01-25 21:02:46 -0300

    blog works locally

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-creted_on')
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
 
 class DetailView(generic.DetailView):
